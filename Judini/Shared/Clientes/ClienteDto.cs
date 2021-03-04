440673da29895a1f2c14e7c97f2735e5ead1b0eb
Franco Lianza <lianza.fl@gmail.com>
2021-03-04 18:05:45 -0300

    Pagina agregar cliente

---
+++ Judini/Shared/Clientes/ClienteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Judini.Shared.Clientes
+{
+    public class ClienteDto
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+    }
+}
